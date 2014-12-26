3969949bcd47e9d4f9e2b281ccad3b5bc8ebf76c
koroket <bmasakazu@gmail.com>
2014-12-26 04:27:12 -0800

    m

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,5 @@
 class UsersController < ApplicationController
 
-  def index
-    @users = User.where(activated: true)
-  end
-
   def show
     @user = User.find(params[:id])
     redirect_to root_url and return unless @user.activated
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
   get 'signup'  => 'users#new'
-  resources :users
+  resources :users, only: [:new, :create, :edit, :update, :show]
 
   get 'password_resets/new'
 
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
 
-  get '/company/:name', to: 'companies#show', as: :show_company
+  get '/c/:name', to: 'companies#show', as: :show_company
 
   resources :companies, only: [:new, :create, :index]
 
